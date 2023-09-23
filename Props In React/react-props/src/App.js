3e86390c8bb7aa4f7e6154d82df8ebc686d6d758
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-09-23 10:16:50 +0530

    Default props in Class Based Components

--- Props In React/react-props/src/App.js
+++ Props In React/react-props/src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App">
       <Student stuname="Krishna!" marks={80} />
       <Student stuname="Balram!" marks={70} />
+      <Student marks={89} />
+      <Student />
+      
     </div>
   );
+}
+
+
+Student.defaultProps = {
+  stuname: "Student",
+  marks: "N.A"
 }
 
 export default App;
