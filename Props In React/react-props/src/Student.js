d0eb67b0cf66d99c01238521404d65d2220e2cfb
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-09-23 10:20:27 +0530

    Props in Functional Components

--- Props In React/react-props/src/Student.js
+++ Props In React/react-props/src/Student.js
@@ -1,19 +1,44 @@
 import React from "react";
 
-class Student extends React.Component {
-    render(){
+// class Student extends React.Component {
+//     render(){
 
-        console.log(this.props);
-        const {stuname, marks} = this.props;
+//         console.log(this.props);
+//         const {stuname, marks} = this.props;
 
-        return (
-            <>
-             <h1>Hello, {stuname}</h1>
-             <p>You have secured {marks} %</p>
-             <hr />
-            </>
-        )
-    }
+//         return (
+//             <>
+//              <h1>Hello, {stuname}</h1>
+//              <p>You have secured {marks} %</p>
+//              <hr />
+//             </>
+//         )
+//     }
+// }
+
+
+/* Functional Based Components */
+// function Student({stuname, marks}) {
+//     return (
+//         <>
+//          <h1>Hello, {stuname}</h1>
+//          <p>You have secured {marks} %</p>
+//         </>
+//     )
+// }
+
+
+function Student(props) {
+
+    const {stuname, marks} = props;
+    
+    return (
+        <>
+         <h1>Hello, {stuname}</h1>
+         <p>You have secured {marks} %</p>
+        </>
+    )
 }
+
 
 export default Student;
